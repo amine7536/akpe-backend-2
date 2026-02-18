12a72d569815ffcff45e34b719253586a75890d8
Amine Benseddik <amine@pixelfactory.io>
2026-02-18 10:03:32 +0100

    test: e2e validation of values.yaml preview flow

--- main.go
+++ main.go
@@ -62,3 +62,4 @@ func handleHealth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
 }
+// e2e test: values.yaml flow 1771405412
